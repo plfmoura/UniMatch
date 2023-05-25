188c0e2dfe17d7a3fdc63980425b57f39655794d
Davi <davijustinocouto@gmail.com>
2023-05-24 21:05:41 -0300

    -styles(CSS): Adicionada a estilização de texto

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -4,7 +4,9 @@ import './register.css'
 export default function Register() {
   return (
     <div className='Register-container'>
-      
+      <div className="descriptionRegister">
+        <a className="description">Certo, vamos prosseguir com seu cadastro, vou precisar de algumas informações...</a>
+      </div>
     </div>
   )
 }
