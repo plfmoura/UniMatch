e738fb1f23a1f34513b53d6d4fc369d02370c1da
Davi <davijustinocouto@gmail.com>
2023-05-25 08:54:49 -0300

    -styles(CSS): Adicionada a estilização de input

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './register.css'
+import Button from '../../components/Button'
 
 export default function Register() {
   return (
     <div className='Register-container'>
-      <div className="descriptionRegister">
+      <spam className="descriptionRegister">
         <a className="description">Certo, vamos prosseguir com seu cadastro, vou precisar de algumas informações...</a>
+      </spam>
+      <div className="input-container">
+        <input className='inputTransparent' type="email" placeholder="Digite seu nome..."></input>
       </div>
+      <div className="input-container"> 
+        <input className='inputTransparent' type="email" placeholder="Aqui seu sobrenome..."></input>
+      </div>
+      <div className="input-container">
+        <input className='inputTransparent' type="email" placeholder="Data de nascimento..."></input>
+      </div>
+      <div className="input-container">
+        <input className='inputTransparent' type="email" placeholder="Como você se idêntifica?"></input>
+      </div>
+
     </div>
   )
 }
