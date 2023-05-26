366c38238b5be7636db8f958e89ecb45eae0b58f
plfmoura <plfmoura96@gmail.com>
2023-05-25 23:29:23 -0300

    feat(home): create page and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
 import Login from './pages/login'
 import Register from './pages/Register'
+import Home from './pages/Home'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const options = {
-    container: {
-      width: '100vw',
-      height: '100vh',
-    }
-  }
+  const [user, setUser] = useState(false)
 
   return (
     <HashRouter>
-      <Routes>
-        <Route exact path="/" element={<Login />}></Route>
-        <Route exact path="/register" element={<Register/>}></Route>
-      </Routes>
+      {user ?
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
+        </Routes>
+        :
+        <Routes>
+          <Route exact path="/home" element={<Home />}></Route>
+        </Routes>
+      }
     </HashRouter>
   )
 }
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './home.css'
+
+export default function Home() {
+  return (
+    <div className='Home-container'>
+        <h1>Home</h1>
+    </div>
+  )
+}
