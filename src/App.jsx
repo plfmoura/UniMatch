931e3acdb0d14b12a2c0a312312c01f92d7131f7
plfmoura <plfmoura96@gmail.com>
2023-05-26 00:17:53 -0300

    fix(app): route error/ replace login path

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { HashRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/login'
 import Register from './pages/Register'
-import Home from './pages/Home'
-import { useEffect, useState } from 'react'
-import BottomNavigation from './components/Navigation/BottomNavigation'
-import TopNavigation from './components/Navigation/TopNavigation'
 
 function App() {
-  const [user, setUser] = useState(false)
 
   return (
     <HashRouter>
-      {/* {user ?
-        <>
-          <TopNavigation />
-          <Routes>
-            <Route exact path="/home" element={<Home />}></Route>
-          </Routes>
-          <BottomNavigation />
-        </>
-        : */}
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
           <Route exact path="/register" element={<Register />}></Route>
         </Routes>
-      {/* } */}
     </HashRouter>
   )
 }
